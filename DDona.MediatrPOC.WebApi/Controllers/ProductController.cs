9ceb15adb7d5e8050209eef04f83561402c66076
Diego Doná <diedona@gmail.com>
2019-10-28 10:19:58 -0300

    controller web api

---
+++ DDona.MediatrPOC.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DDona.MediatrPOC.Domain.Command.Product;
+using DDona.MediatrPOC.Domain.Repository;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DDona.MediatrPOC.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IMediator mediator, IProductRepository productRepository)
+        {
+            _mediator = mediator;
+            _productRepository = productRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(CreateProductCommand command)
+        {
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+    }
+}
