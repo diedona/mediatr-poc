d303bef0d83d906b1d2ae913ce956a53633853c0
Diego Doná <diedona@gmail.com>
2019-10-28 09:06:34 -0300

    Add project files.

---
+++ DDona.MediatrPOC.Domain/Entity/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDona.MediatrPOC.Domain.Entity
+{
+    public class Product : Entity
+    {
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; set; }
+    }
+}
