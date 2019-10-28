ac37c075323037bf6711800c0a0c7ac0148b0e73
Diego Doná <diedona@gmail.com>
2019-10-28 12:04:42 -0300

    one more notification handler

---
+++ DDona.MediatrPOC.Domain/EventsHandler/ProductEvent/DatabaseProductHandler.cs
@@ -0,0 +1,21 @@
+﻿using DDona.MediatrPOC.Domain.Notification.ProductNotification;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DDona.MediatrPOC.Domain.EventsHandler.ProductEvent
+{
+    public class DatabaseProductHandler : INotificationHandler<ProductCreatedNotification>
+    {
+        public Task Handle(ProductCreatedNotification notification, CancellationToken cancellationToken)
+        {
+            return Task.Run(() =>
+            {
+                Console.WriteLine($"- from Database - we should consider REALLY inserting the {notification.Id}...");
+            });
+        }
+    }
+}
